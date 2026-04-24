c076768d3601c5befcde01d5e3e127e654f5e614
Edisson <parradoreyes@gmail.com>
2026-04-23 20:17:33 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,7 @@
 # Hola yo soy Juan reyes y este es mi aporte
+# este el archivo base para empezar con el desarrollo del codigo
+# Integrantes:  Edisson Parrado
+#               Juan Reyes
+
+
+# este es el aporte de juan
